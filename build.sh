20a48fcf60ee0fc85728ff7c481d34058c0d9024
Toxblh <toxblh@gmail.com>
2018-04-25 17:17:32 +0100

    remove dead code. Add scripts for create releases.

---
+++ build.sh
@@ -0,0 +1,23 @@
+NAME='GitHubListener'
+
+rm -r Release 2>/dev/null
+
+xcodebuild archive \
+	-scheme "$NAME" \
+	-archivePath Release/App.xcarchive
+
+xcodebuild \
+	-exportArchive \
+	-archivePath Release/App.xcarchive \
+	-exportOptionsPlist export-options.plist \
+	-exportPath Release
+
+cd Release
+rm -r App.xcarchive
+
+# Prerequisite: npm i -g create-dmg
+# create-dmg "${NAME}.app"
+
+VERSION=`mdls -raw -name kMDItemVersion $NAME.app`
+echo $VERSION
+zip -r "${NAME}v${VERSION}.zip" "${NAME}.app"
